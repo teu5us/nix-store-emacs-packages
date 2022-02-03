35455ff9a2d252a53f8bf47ac70fd8a704b1499a
Pavel Stepanov <paulkreuzmann@gmail.com>
2022-02-03 09:40:57 +0600

    call override from super

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 self: super:
 
 {
-  emacsPackages = self.emacsPackages.overrideScope' (self': super': {
+  emacsPackages = super.emacsPackages.overrideScope' (self': super': {
     nix-store-emacs-packages = self'.trivialBuild rec {
       pname = "nix-store-emacs-packages";
       ename = "nix-store-emacs-packages";
