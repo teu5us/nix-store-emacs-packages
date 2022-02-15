487aa4cd5c0d890f670c38064616b86282042b91
teu5us <42467493+teu5us@users.noreply.github.com>
2022-02-15 12:06:51 +0600

    export the overlay

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: rec {
+    overlay = import ./default.nix;
     nixosModule =
       { config, lib, pkgs, utils, ... }: {
         nixpkgs.overlays = [
-          (import ./default.nix)
+          overlay
         ];
       };
   };
